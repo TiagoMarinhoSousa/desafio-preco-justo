1fa21644e0d4576a41c81aa73e9e28bc1446009f
Tiago Marinho Sousa <tiagomarinho9101@gmail.com>
2025-11-12 16:24:28 -0300

    refactor: organiza interfaces e serviços em pastas separadas (models e services)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PostListPage } from './posts/post-list.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PostListPage,
+  },
+];
+
