4d194e09c3697a7d28788d6dcdfd5e112aaf771c
Tiago Marinho Sousa <tiagomarinho9101@gmail.com>
2025-11-12 16:47:25 -0300

    fix: adiciona provideHttpClient para habilitar HttpClient em projeto standalone

--- src/app/posts/post-list.page.ts
+++ src/app/posts/post-list.page.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { PostService } from './services/post.service';
+import { Post } from './models/post.model';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-post-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './post-list.page.html',
-  styleUrls: ['./post-list.page.css']
+  styleUrls: ['./post-list.page.css'],
 })
 export class PostListPage {
+  posts$!: Observable<Post[]>;
 
+  constructor(private postService: PostService) {}
+
+  ngOnInit(): void {
+    this.posts$ = this.postService
+      .getPosts()
+      .pipe(map((posts) => posts.slice(0, 10)));
+  }
 }
